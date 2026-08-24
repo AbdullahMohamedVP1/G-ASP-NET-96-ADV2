98e6ab690fcf7f7d0356c2cd5e636dc444b88cd1
AbdullahMohamedVP1 <boda12834@gmail.com>
2026-08-25 00:25:08 +0300

    Submission Guidelines

--- AssigmentADV2/ProductSearcher.cs
+++ AssigmentADV2/ProductSearcher.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 namespace AssigmentADV2
 {
     public static class ProductSearcher{
+        // Func<Product,bool>: بترجع true/false لتحديد هل المنتج مطابق للشرط ولا لأ
         public static List<Product> SearchProducts(List<Product> products, Func<Product, bool> predicate){
             var result = new List<Product>();
             foreach (var product in products){
