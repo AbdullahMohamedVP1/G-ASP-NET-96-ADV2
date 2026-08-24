544b3dbd3344dd210dad2cc16fda7ec1e029f7cb
AbdullahMohamedVP1 <boda12834@gmail.com>
2026-08-25 00:03:16 +0300

    Task 01: Smart Product Search

---
+++ AssigmentADV2/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssigmentADV2
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; } // "Electronics", "Clothing", "Food", "Books"
+        public double Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
